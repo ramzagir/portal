8804340e19d84ebc0e4b1bfde87897ff5469c0b0
user <ozil@ro.ru>
2017-12-05 04:33:12 +0400

    Add Documents

--- portal.web/src/main/java/ru/neoflex/DocumentsBean.java
+++ portal.web/src/main/java/ru/neoflex/DocumentsBean.java
@@ -3,10 +3,12 @@ package ru.neoflex;
 
 
 import ru.neoflex.domain.Documents.Documents;
+import ru.neoflex.service.DocumentsService;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.model.SelectItem;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ import java.util.List;
 @Named
 @SessionScoped
 public class DocumentsBean implements Serializable{
+
     private String value;
     private String type;
     private boolean confirm = false;                // флаг для определения успешной записи в БД
@@ -22,7 +25,7 @@ public class DocumentsBean implements Serializable{
 
 
     @EJB
-    DocumentsServiceImpl documentsServiceImpl;
+    DocumentsService documentsService;
 
     // getters and setters
     public String getValue() {
@@ -60,22 +63,21 @@ public class DocumentsBean implements Serializable{
     // добавляем документ типа указанного в поле type
     public void addDocuments(){
         if(type.equals("passport"))
-            confirm = documentsServiceImpl.addPassport(value);
+            confirm = documentsService.addPassport(value);
         else if (type.equals("diplom"))
-            confirm = documentsServiceImpl.addDiplom(value);
+            confirm = documentsService.addDiplom(value);
         else if (type.equals("military"))
-            confirm = documentsServiceImpl.addMilitary(value);
+            confirm = documentsService.addMilitary(value);
         else if (type.equals("tempPassport"))
-            confirm = documentsServiceImpl.addTempPassport(value);
+            confirm = documentsService.addTempPassport(value);
     }
-
 
     /**
      * Выводим список документов типа type
      * @return Спикок документов указанного типа
      */
     public List<Documents> showDocuments(){
-        return documentsServiceImpl.showDocuments(type);
+        return documentsService.showDocuments(type);
     }
 
     /**
@@ -83,6 +85,6 @@ public class DocumentsBean implements Serializable{
      * @return Список типов документов
      */
     public List<String> showTypeOfDocuments(){
-        return documentsServiceImpl.showTypeOfDocuments();
+        return documentsService.showTypeOfDocuments();
     }
 }
